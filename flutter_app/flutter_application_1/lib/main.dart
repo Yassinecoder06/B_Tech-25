e96503035bb9d3f47171a8fd5059e52c1ce470a1
Yassinecoder06 <yassinejemmali45@gmail.com>
2025-01-21 19:35:58 +0100

    Initial commit

---
+++ flutter_app/flutter_application_1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false;
+      home: LoginPage();
+    )
+  }
+}
+
+void main() {
+  runApp(App());
+}
